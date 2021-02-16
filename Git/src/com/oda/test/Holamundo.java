77439ee28f9493cdd2674410d9b70c85ddef5372
Oliver, Butom <butom.oliver797@googlemail.com>
2021-02-16 15:01:37 +0100

    Pacertest was refined

--- Git/src/com/oda/test/Holamundo.java
+++ Git/src/com/oda/test/Holamundo.java
@@ -8,7 +8,10 @@ public class Holamundo {
 		System.out.println("New line 2.");
 		System.out.println("anotha one");
 		System.out.println("your Test is in another Branch");
-		System.out.println("The FitnessGram PACER Test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The test is used to measure a student's aerobic capacity as part of the FitnessGram assessment. Students run back and forth as many times as they can, each lap signaled by a beep sound.");
+		System.out.println("The FitnessGram PACER Test is a multistage aerobic capacity test that progressively"
+				+ " gets more difficult as it continues. The test is used to measure a student's aerobic capacity "
+				+ "as part of the FitnessGram assessment. Students run back and forth as many times as they can, "
+				+ "each lap signaled by a beep sound.");
 	}
 
 }
